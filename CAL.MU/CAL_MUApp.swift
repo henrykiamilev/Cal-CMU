a5c4e21ce3b1302b2df51931af84472a69d00c45
Henry Kiamilev <henrykiamilev@gmail.com>
2026-02-19 16:12:20 -0500

    Initial Commit

---
+++ CAL.MU/CAL_MUApp.swift
@@ -0,0 +1,17 @@
+//
+//  CAL_MUApp.swift
+//  CAL.MU
+//
+//  Created by Henry Kiamilev on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct CAL_MUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
